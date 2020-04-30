603d7556c54e2062b83a7608a9cc516ce63f71ec
ARawles <adamrawles@hotmail.co.uk>
2020-04-30 08:27:15 +0100

    Improved BMRSr colourscheme

--- main.R
+++ main.R
@@ -5,9 +5,18 @@ if (!library("hexSticker", quietly = TRUE, logical.return = TRUE)) {
 if (!library("ggplot2", quietly = TRUE, logical.return = TRUE)) {
   install.packages("ggplot2")
 }
+
+if (!library("rsvg", quietly = TRUE, logical.return = TRUE)) {
+  install.packages("rsvg")
+}
 library("hexSticker")
 library("ggplot2")
+library("rsvg")
 
 bmrsr_image <- sticker("images/turbines.png", package = "BMRSr", p_size=20, s_x=1, s_y=.75, s_width=.4, s_height = .4,
-                       h_fill = "white", h_color = "purple", p_color = "purple",
+                       h_fill = "#a2f2a9", h_color = "#003304", p_color = "#003304",
                                filename="logos/bmrsr.png")
+
+teacher_image <- sticker("images/chalkboard.svg", package = "teacheR", p_size=20, s_x=1, s_y=.75, s_width=.4, s_height = .4,
+                      h_fill = "#d1dcf0", h_color = "#0b41a3", p_color = "#0b41a3",
+                      filename="logos/teacher.png")

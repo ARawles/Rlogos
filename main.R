0be19a8b2dc75425c386ecd8323c9e783171f172
ARawles <adamrawles@hotmail.co.uk>
2020-05-18 10:01:07 +0100

    Added opeRate logo

--- main.R
+++ main.R
@@ -20,3 +20,7 @@ bmrsr_image <- sticker("images/turbines.png", package = "BMRSr", p_size=20, s_x=
 teacher_image <- sticker("images/chalkboard.svg", package = "teacheR", p_size=20, s_x=1, s_y=.75, s_width=.4, s_height = .4,
                       h_fill = "#d1dcf0", h_color = "#0b41a3", p_color = "#0b41a3",
                       filename="logos/teacher.png")
+
+operate_image <- sticker("images/crane.svg", package = "opeRate", p_size = 20, s_x = 1, s_y = .75, s_width = .4,
+                         s_height = .4, h_fill = "#ebb16a", h_color = "#bd8a4b", p_color = "#bd8a4b",
+                         filename = "logos/operate.png")

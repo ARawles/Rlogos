2282aaa2f71cd9bb04e35374d573e1a1b5297272
ARawles <adamrawles@hotmail.co.uk>
2020-05-20 11:36:43 +0100

    Added faviconGeneratoR logo

--- R/main.R
+++ R/main.R
@@ -16,24 +16,36 @@ if (!library("svglite", quietly = TRUE, logical.return = TRUE)) {
 if (!library("httr", quietly = TRUE, logical.return = TRUE)) {
   install.packages("httr")
 }
+if (!library("remotes", quietly = TRUE, logical.return = TRUE)) {
+  install.packages("remotes")
+}
 
 library("hexSticker")
 library("ggplot2")
 library("rsvg")
 library("svglite")
 library("httr")
+library("remotes")
+
+if (!library("faviconGeneratoR", quietly = TRUE, logical.return = TRUE)) {
+  remotes::install_github("ARawles/faviconGeneratoR")
+}
 
 bmrsr_image <- sticker("images/turbines.png", package = "BMRSr", p_size=20, s_x=1, s_y=.75, s_width=.4, s_height = .4,
                        h_fill = "#a2f2a9", h_color = "#003304", p_color = "#003304",
                                filename="logos/bmrsr.png")
 
-teacher_image <- sticker("images/chalkboard.svg", package = "teacheR", p_size=20, s_x=1, s_y=.75,# s_width=.4, s_height = .4,
+teacher_image <- sticker("images/mortarboard.svg", package = "teacheR", p_size=20, s_x=1, s_y=.75, s_width=.4, s_height = .4,
                       h_fill = "#d1dcf0", h_color = "#1677D2", p_color = "#1677D2",
                       filename="logos/teacher.png")
 
-operate_image <- sticker("images/crane.svg", package = "opeRate", p_size = 5, s_x = 1, s_y = .75, #s_width = .1, s_height = .4,
+operate_image <- sticker("images/crane.svg", package = "opeRate", p_size = 20, s_x = 1, s_y = .75, s_width = .4, s_height = .4,
                          h_fill = "#ffa8a8", h_color = "#bf4545", p_color = "#bf4545", dpi = 300,
                          filename = "logos/operate.png")
+
+favicongenerator_image <- sticker("images/paintbrush.svg", package = "faviconGeneratoR", p_size = 15, s_x = 1, s_y = .80, s_width = .3, s_height = .3,
+                                  h_fill = "#ba61ff", h_color = "#67338f", p_color = "#67338f", dpi = 300,
+                                  filename = "logos/favicongenerator.png")
 
 
 ## TO DO: Add automated favicon creation
